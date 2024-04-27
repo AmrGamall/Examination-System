2c2caf774bd9d418acabfc69b75c61fdc14707df
amr <amrgemy00@gmail.com>
2024-04-27 13:18:54 +0300

    upload

---
+++ CSharp Exam/MCQQuestion.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Exam
+{
+    public class MCQQuestion : Question
+    {
+        public override string Header => "MCQ Question";
+
+        public MCQQuestion()        
+        {
+            AnswerList = new Answer[3]; 
+        }
+        public override void AddQuestions()
+        {
+            // Header
+            Console.WriteLine(Header);
+
+            // Body
+            Console.WriteLine("Enter body of Question");
+            Body = Console.ReadLine();
+
+            // Marks
+            int mark;
+            do
+            {
+                Console.WriteLine("enter marks of question");
+
+            }while(!int.TryParse(Console.ReadLine(), out mark) || mark < 1);
+
+            Marks = mark;           
+
+            // Answers of question
+
+            Console.WriteLine("enter answers of question:");
+            for (int i = 0; i < 3; i++)
+            {
+                AnswerList[i] = new Answer
+                {
+                    AnswerId= i+1
+                };
+                            
+                Console.WriteLine($"Enter answer number{i+1}:");
+                AnswerList[i].AnswerText = Console.ReadLine();
+            }
+
+
+            // Right Answer
+
+            int rightAnswerId;
+            do
+            {
+                Console.WriteLine("enter id of the right answer:");
+
+            } while (!int.TryParse(Console.ReadLine(), out rightAnswerId) || rightAnswerId < 1 || rightAnswerId > 3);
+
+            RightAnswer.AnswerId = rightAnswerId;
+            RightAnswer.AnswerText = AnswerList[rightAnswerId-1].AnswerText;
+
+            Console.Clear();
+        }
+    }
+}
---
+++ CSharp Exam/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace CSharp_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new Subject(1,"C#");
+
+            subject.CreateExam();
+            char c;
+            do
+            {
+                Console.WriteLine("Do You Want to start the Exam (Y | N):");
+
+
+            } while (!char.TryParse(Console.ReadLine(), out c));
+
+            if(c == 'Y' || c == 'y')
+            {
+                Console.Clear();
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                subject.SubjectExam.ShowExam();
+                Console.WriteLine($"Time Taken:{sw.Elapsed}");
+            }
+            else
+                Console.WriteLine("Thank You!");
+        }
+    }
+}
